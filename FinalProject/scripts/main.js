8c9c2c3966f5aed3576b3e085737aa79d128b8f1
RRekte <rrekte@charter.net>
2026-04-26 16:22:24 -0600

    lkjhb

--- FinalProject/scripts/main.js
+++ FinalProject/scripts/main.js
@@ -1,3 +1,4 @@
+let y = 0; // starting at the top of the canvas
 
 <canvas id="myCanvas" width="400" height="400"></canvas>
 const canvas = document.getElementById("myCanvas");
@@ -8,6 +9,23 @@ ctx.shadowBlur = 20;
 ctx.beginPath();
 ctx.arc(150, 150, 40, 0, Math.PI * 2);
 ctx.fill();
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Add this line that erases the previous frame
+
+  ctx.beginPath();
+  ctx.arc(150, y, 40, 0, Math.PI * 2); // use y instead of 150 so it moves along the vertical axis
+  ctx.fill();
+
+  y += 1; // tiny downward movement
+}
+function loop() {
+  draw();
+  requestAnimationFrame(loop);
+}
+
+loop(); // start the animation
+
+
 
 
 
